d464d2a8810e9a4ebaeeb68765b1ceb731f72020
kartik589123 <meta619012@gmail.com>
2023-04-04 15:27:26 +0530

    domtaskk

---
+++ domtask.js
@@ -0,0 +1,20 @@
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "123";
+// console.log(document.doctype);
+// console.log(document.all[10]);
+// //document.all[10].textContent = "hello";
+
+console.log(document.getElementById("header-title"));
+var h = document.getElementById("main-header");
+console.log(h);
+// h.textContent = "hello";
+// h.innerText = "dsad€sd";
+// h.innerHTML = "<h3>hello</h3>";
+
+h.style.borderBottom = "solid 5px #000";
+
+var add = document.getElementsByClassName("title");
+add[0].style.fontWeight = "bold";
+add[0].style.color = "green";
