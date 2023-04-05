9e8a4a0855c606f3d698d11c331c53815b4f5e64
kartik589123 <meta619012@gmail.com>
2023-04-05 08:50:49 +0530

    getelementsbytagname

--- domtask.js
+++ domtask.js
@@ -15,16 +15,26 @@ console.log(h);
 
 h.style.borderBottom = "solid 5px #000";
 
-var add = document.getElementsByClassName("title");
-add[0].style.fontWeight = "bold";
-add[0].style.color = "green";
+// var add = document.getElementsByClassName("title");
+// add[0].style.fontWeight = "bold";
+// add[0].style.color = "green";
 
-var items = document.getElementsByClassName("list-group-item");
+// var items = document.getElementsByClassName("list-group-item");
 
-console.log((items[0].textContent = "hello"));
+// console.log((items[0].textContent = "hello"));
 
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
-for (item of items) {
+// for (item of items) {
+//   item.style.fontWeight = "bold";
+// }
+
+var li = document.getElementsByTagName("li");
+
+console.log((li[0].textContent = "hello"));
+
+li[2].style.backgroundColor = "green";
+
+for (item of li) {
   item.style.fontWeight = "bold";
 }
