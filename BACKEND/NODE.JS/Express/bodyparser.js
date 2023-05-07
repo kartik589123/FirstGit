fcd6428c3a2c2b8d60b8798305b8c4603823c88a
kartik589123 <meta619012@gmail.com>
2023-05-07 11:17:00 +0530

    BodyParser

---
+++ BACKEND/NODE.JS/Express/bodyparser.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send('<h1 style="color:red;">Hello from Exprees.js</h1>');
+});
+
+app.listen(4000, () => console.log("server 4000 is running"));
