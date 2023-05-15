750b14437e20826419139fa556f4f430a4d88cf3
kartik589123 <meta619012@gmail.com>
2023-05-15 12:24:24 +0530

    Edit and delete

---
+++ Dummy Registration page with express/SHOP/data/Router.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+const errorcontroller = require("../Controllers/error");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const AdminRoutes = require("./Admin");
+
+const Shoproutes = require("../../../SHOP/Shop");
+
+const contactus = require("./contact");
+
+const successs = require("../../../SHOP/success");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "../", "public"))); ///to use for accessing file system from website
+
+app.use("/admin", AdminRoutes);
+
+app.use(Shoproutes);
+
+app.use(contactus);
+
+app.use(successs);
+
+app.use(errorcontroller.get404page);
+
+app.listen(4000, () => console.log("server 4000 is running"));
---
+++ Edit and Delete/models/cart.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  //fetch the previous cart
+
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      //Analyze the cart => Find existing quantity
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      //Add new product / increase quantity
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + Number(productPrice);
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
